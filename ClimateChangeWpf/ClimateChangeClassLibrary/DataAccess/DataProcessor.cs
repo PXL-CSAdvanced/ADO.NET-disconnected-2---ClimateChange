ee51a35bde36764643405de7f239d481f9e2593e
Sander De Puydt <pxldemo@gmail.com>
2025-03-23 18:42:32 +0100

    Opdracht aangemaakt

---
+++ ClimateChangeWpf/ClimateChangeClassLibrary/DataAccess/DataProcessor.cs
@@ -0,0 +1,76 @@
+﻿using ClimateChangeClassLibrary.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClimateChangeClassLibrary.DataAccess
+{
+    public static class DataProcessor
+    {
+        private const string CountriesTableName = "Countries";
+        private const string TempChangeTableName = "TempChange";
+        private const string TempChangeFilePath = "resources/Temperature_change.csv";
+        private const string CountriesFilePath = "resources/Country_codes_and_flags.csv";
+
+        private static DataSet _climateChangeDataSet;
+
+        #region AanmakenDataSet
+        static DataProcessor()
+        {
+            InitializeDataSet();
+        }
+
+        public static void InitializeDataSet()
+        {
+            throw new NotImplementedException();
+            // TODO: Gebruik de methodes InitializeCountriesDataTable() en InitializeTempChangeDataTable()
+            // om er voor te zorgen dat de DataTables in deze DataSet correct aangemaakt zijn.
+        }
+
+        private static DataTable InitializeCountriesDataTable()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static DataTable InitializeTempChangeDataTable()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #region VullenDataSet
+        private static void AddRowsToDataTableFromFile(DataTable dt, string filename)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        public static DataView GetTempChangeDataView()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static DataView GetCountriesDataView()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<TempChange> GetWorstYearsAfter2000()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<Country> GetCountries()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<TempChange> GetTempChangesByCountryName(Country country)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ClimateChangeWpf/ClimateChangeWpf/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using ClimateChangeClassLibrary.DataAccess;
+using ClimateChangeClassLibrary.Entities;
+using System.Diagnostics.Metrics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClimateChangeWpf;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        // TODO: koppel DataGrid's
+
+        LoadComboBoxItems();
+        CountriesComboBox.SelectedIndex = 0;
+
+        // TODO: vul WorstYearsListBox met een gepaste List van TempChange objecten
+    }
+
+    private void LoadComboBoxItems()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void CountriesComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void LoadListBoxItemsByCountry(Country country)
+    {
+        throw new NotImplementedException();
+    }
+
+    private ListBoxItem CreateListBoxItem(TempChange tc, bool isAddingCountryName)
+    {
+        throw new NotImplementedException();
+    }
+}
